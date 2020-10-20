78a01da71651f80f9b502577efe8d748005cd60b
ManonFargues <manon.fargues@ynov.com>
2020-10-20 15:11:00 +0200

    :sparkles: add course list

---
+++ liste-course/lib/widget/HomeController.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+
+import 'ListModel.dart';
+
+class HomeController extends StatefulWidget {
+  @override
+  _HomeControllerState createState() => _HomeControllerState();
+}
+
+
+class _HomeControllerState extends State<HomeController> {
+  List<Produit> produit = [
+    Produit(nom: 'Salade', prix: '5'),
+    Produit(nom: 'Paté', prix: '5'),
+    Produit(nom: 'Granola', prix: '5'),
+    Produit(nom: 'Steak', prix: '5'),
+    Produit(nom: 'Ice tea', prix: '5'),
+    Produit(nom: 'Poisson', prix: '5'),
+    Produit(nom: 'Pain', prix: '5'),
+    Produit(nom: 'Lait', prix: '5'),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blueGrey,
+        title: Text(
+          'Ma liste de course'.toUpperCase(),
+        ),
+      ),
+
+      /*Liste des produits*/
+      body: ListView.builder(
+          itemCount: produit.length,
+          itemBuilder: (context, index) {
+            return Padding(
+              padding: EdgeInsets.symmetric(vertical: 4.0, horizontal: 4.0),
+              child: Card(
+                elevation: 0,
+                color: Colors.blueGrey,
+                child: ListTile(
+                  title: Text(produit[index].nom,
+                  ),
+                ),
+              ),
+            );
+          }),
+    );
+  }
+}
