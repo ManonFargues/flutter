0c2070d0174498f28bfca13c9ce358965e7960b3
ManonFargues <manon.fargues@ynov.com>
2020-10-20 15:13:17 +0200

    :sparkles: initialRoute

---
+++ liste-course/lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'widget/HomeController.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'MA LISTE DE COURSE',
+      theme: ThemeData(
+          primarySwatch: Colors.blue,
+          visualDensity: VisualDensity.adaptivePlatformDensity,
+          pageTransitionsTheme: PageTransitionsTheme(builders: {
+            TargetPlatform.android: CupertinoPageTransitionsBuilder(),
+            TargetPlatform.iOS: CupertinoPageTransitionsBuilder()
+          })),
+      home: HomeController(),
+    );
+  }
+}
